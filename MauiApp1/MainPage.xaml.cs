44fa05cb4fd5b1d495a3bdb75cc2c6cf6d01ed51
Tumas <106020074+thommy2k@users.noreply.github.com>
2023-10-22 10:38:17 +0800

    Add project files.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    public void onVerticalStart(object sender, EventArgs e)
+    {
+        target.VerticalOptions = LayoutOptions.Start;
+    }
+
+    public void onVerticalCenter(object sender, EventArgs e)
+    {
+        target.VerticalOptions = LayoutOptions.Center;
+    }
+    public void onVerticalEnd(object sender, EventArgs e)
+    {
+        target.VerticalOptions = LayoutOptions.End;
+    }
+    public void onHorizontalStart(object sender, EventArgs e)
+    {
+        target.HorizontalOptions = LayoutOptions.Start;
+    }
+    public void onHorizontalCenter(object sender, EventArgs e)
+    {
+        target.HorizontalOptions = LayoutOptions.Center;
+    }
+    public void onHorizontalEnd(object sender, EventArgs e)
+    {
+        target.HorizontalOptions = LayoutOptions.End;
+    }
+}
+
